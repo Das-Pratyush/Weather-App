f5bcaab17ebe1edf3607399720b855c1d414da8e
Pratyush Das <147391420+Das-Pratyush@users.noreply.github.com>
2024-04-11 17:46:07 +0530

    First Commit

---
+++ WeatherApp/index.js
@@ -0,0 +1,47 @@
+const apiKey = "e5e8836309ff736f49d63d1e02ab8b4b"
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e5e8836309ff736f49d63d1e02ab8b4b
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    if (response.status) {
+        document.querySelector(".error").style.display = "block"
+        document.querySelector(".weather").style.display = "none"
+    } else {
+        let data = await response.json()
+
+        document.querySelector(".city").innerHTML = data.name
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png"
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png"
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png"
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png"
+        }
+        document.querySelector(".weather").style.display = "block"
+        document.querySelector(".error").style.display = "none"
+
+    }
+   
+}
+searchBtn.addEventListener("click", function () {
+    checkWeather(searchBox.value)
+})
